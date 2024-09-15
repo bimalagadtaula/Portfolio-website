7b10153dce63cb33c1d586be28c96cf4df14f6bb
Bmala <bimlagd@gmail.com>
2024-09-15 02:14:32 -0400

    started

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+import Hero from "../components/main/Hero";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20 h-[850px]" >
+     <Hero/>
+   
+      </div>
+
+    </main>
+  );
+}
---
+++ custom.d.ts
@@ -0,0 +1,17 @@
+// custom.d.ts
+declare module '*.webm' {
+    const src: string;
+    export default src;
+  }
+  
+  declare module '*.mp4' {
+    const src: string;
+    export default src;
+  }
+  
+  // Similarly for images or other media types
+  declare module '*.png' {
+    const src: string;
+    export default src;
+  }
+  
